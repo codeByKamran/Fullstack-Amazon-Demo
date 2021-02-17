c86f405ac29f704cadf042e757c5598fa60ca031
Kamran Ashraf <79129427+codeByKamran@users.noreply.github.com>
2021-02-17 11:22:50 +0500

    Update Copyright.js

--- src/Components/Copyright.js
+++ src/Components/Copyright.js
@@ -6,8 +6,8 @@ const CopyrightSection = () => {
     <div className="copyright__section">
       <h1>
         ©This <strong>Amazon Clone</strong> is Built by
-        <a href="https://www.azharzaman.com" target="_blank">
-          Azhar Zaman
+        <a href="https://kamran-personal-portfolio.netlify.app/" target="_blank">
+          Kamran A.
         </a>
         with <strong>React</strong> to Showcase Skills©
       </h1>
